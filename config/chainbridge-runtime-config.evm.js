8769827694c792c04be1429d0aef75b1ab40f5b7
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2023-02-12 21:46:29 +0000

    Update chainbridge-runtime-config.evm.js

--- config/chainbridge-runtime-config.evm.js
+++ config/chainbridge-runtime-config.evm.js
@@ -1,46 +1,48 @@
 window.__RUNTIME_CONFIG__ = {
   CHAINBRIDGE: {
     chains: [
-      {
-        chainId: 0,
-        networkId: 4,
-        name: "Ethereum - Rinkeby",
-        decimals: 18,
-        bridgeAddress: "0x691DCf269FDb2B9e4229d27fEd2FA7cEeBF660C8",
-        erc20HandlerAddress: "0x4051C29d0545923554E3aBae5a31b5fb27e5949f",
-        rpcUrl: "http://localhost:4000",
-        type: "Ethereum",
-        nativeTokenSymbol: "ETH",
-        tokens: [
-          {
-            address: "0x6829D0dA9f03338703E3d9fcAD6DD308202faf46",
-            name: "an ERC20",
-            symbol: "ERC20",
-            imageUri: "WETHIcon",
-            resourceId:
-              "0x000000000000000000000000000000e389d61c11e5fe32ec1735b3cd38c69500",
-          },
-        ],
-      },
-      {
-        chainId: 1,
-        networkId: 5,
-        name: "Ethereum - Goerli",
-        decimals: 18,
-        bridgeAddress: "0xF014f5c6B88C7D74c846eEEEE29c55d6BB51D9B0",
-        erc20HandlerAddress: "0xa52624ac54165113913D37184AA5957DB99CF2C9",
-        rpcUrl: "http://localhost:4001",
-        type: "Ethereum",
-        nativeTokenSymbol: "ETH",
-        tokens: [
-          {
-            address: "0x3e9a09aBB17509951Ea6F6E221700660017683cb",
-            name: "an ERC20",
-            symbol: "ERC20",
-            imageUri: "WETHIcon",
-            resourceId:
-              "0x000000000000000000000000000000e389d61c11e5fe32ec1735b3cd38c69500",
-          },
+    {
+      chainId: 0,
+      networkId: 42,
+      name: "chikochain",
+      decimals: 18,
+      bridgeAddress: "0x4b552F62A4Db1f31C59c93E8Cb5A112a410598Ef",
+      erc20HandlerAddress: "0x3C4BcD08649897975123Cd8E47b2B34fB6e06D57",
+      rpcUrl: "http://70.34.216.42:9933",
+      type: "Ethereum",
+      blockExplorer: "http://95.179.194.226:3000/?rpc=ws%3A%2F%2F70.34.216.42%3A9944#/explorer",
+      nativeTokenSymbol: "CHK",
+      tokens: [
+        {
+          address: "0x2e5E530dC2C6b2A8f214ee929dC4a302575881A9",
+          name: "WrappedChikoToken",
+          symbol: "WCHK",
+          imageUri: ETHIcon,
+          resourceId:
+            "0x000000000000000000000000000000c76ebe4a02bbc34786d860b355f5a5ce00",
+        },
+      ],
+    },
+    {
+      chainId: 1,
+      networkId: 137,
+      name: "polygon",
+      decimals: 18,
+      bridgeAddress: "0x006f485B4216759cfb8979DE2E4974f74c95585D",
+      erc20HandlerAddress: "0xaFfCFf9AA352E8f3960e2B9538164053F8C9E264",
+      rpcUrl: "https://polygon-mainnet.g.alchemy.com/v2/Sziok2o64OsCpH9HP339wQdHe-akTGFi",
+      type: "Ethereum",
+      blockExplorer: "https://polygonscan.com/",
+      nativeTokenSymbol: "MATIC",
+      tokens: [
+        {
+          address: "0x9e1a245707799e747B4482E965B18BDd7cB4df57",
+          name: "WrappedChikoToken",
+          symbol: "WCHK",
+          imageUri: ETHIcon,
+          resourceId:
+            "0x000000000000000000000000000000c76ebe4a02bbc34786d860b355f5a5ce00",
+        },
         ],
       },
     ],
