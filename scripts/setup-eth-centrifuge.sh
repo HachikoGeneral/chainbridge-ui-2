25bf8f080364b59961a3fd5c8031a306509e3442
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2023-02-13 01:29:47 +0000

    Update setup-eth-centrifuge.sh

--- scripts/setup-eth-centrifuge.sh
+++ scripts/setup-eth-centrifuge.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cb-sol-cli deploy --all --relayerThreshold 1
-cb-sol-cli bridge register-resource --resourceId "0x00000000000000000000000000000009e974040e705c10fb4de576d6cc261900" --targetContract "0x21605f71845f372A9ed84253d2D024B7B10999f4"
-cb-sol-cli bridge set-burn --tokenContract "0x21605f71845f372A9ed84253d2D024B7B10999f4"
-cb-sol-cli erc20 add-minter --minter "0x3167776db165D8eA0f51790CA2bbf44Db5105ADF"
+cb-sol-cli bridge register-resource --resourceId "0x000000000000000000000000000000c76ebe4a02bbc34786d860b355f5a5ce00" --targetContract "0x006f485B4216759cfb8979DE2E4974f74c95585D"
+cb-sol-cli bridge set-burn --tokenContract "0x9e1a245707799e747B4482E965B18BDd7cB4df57"
+cb-sol-cli erc20 add-minter --minter "0xaFfCFf9AA352E8f3960e2B9538164053F8C9E264"
